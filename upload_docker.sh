ce236db48325b0dd66454e99f20439e89f42020c
EC2 Default User <ec2-user@ip-172-31-5-200.ec2.internal>
2022-07-26 07:00:38 +0000

    bash scripts command

---
+++ upload_docker.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env bash
+# This file tags and uploads an image to Docker Hub
+
+# Assumes that an image is built via `run_docker.sh`
+
+# Step 1:
+# Create dockerpath
+dockerpath=martinsops/ml-app
+export dockerpath
+
+# Step 2:  
+# Authenticate & tag
+echo "Docker ID and Image: $dockerpath"
+docker tag ml-app:latest $dockerpath:latest
+
+# Step 3:
+# Push image to a docker repository
+docker push $dockerpath
